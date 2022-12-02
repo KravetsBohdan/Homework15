4b608146dc8840975543c5fd395adf6845bf36ec
KravetsBohdan <bodyakravets@gmail.com>
2022-12-02 20:36:15 +0200

    Add tests to FunctionalUtilities

--- src/main/java/com/bkravets/App.java
+++ src/main/java/com/bkravets/App.java
@@ -1,8 +1,10 @@
 package com.bkravets;
 
 
+import com.bkravets.utlities.FunctionalUtilities;
+
 public class App {
     public static void main( String[] args ) {
-
+        FunctionalUtilities.randomPrinter.accept(10);
     }
 }
